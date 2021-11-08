117c5721e2dfce84470fd2d96e92db0648eeddae
haagor <sim.paris06@gmail.com>
2021-11-08 12:17:15 +0100

    endrinmaster and init readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# degAOS
+# degAOS - esperance de dommage AOS
+
+![](https://github.com/haagor/degAOS/img/kharadron.png)
+
+## ENDRINMASTER WITH DIRIGIBLE SUIT
+> enemy have no save
+![](https://github.com/haagor/degAOS/img/endrinmasterDirigible.png)
+
+---
+
+Simon P
--- src/fight.py
+++ src/fight.py
@@ -1,11 +1,47 @@
 import random
+import numpy
+import matplotlib.pyplot as plt
 
 
-def attack(nb_attack, to_hit, to_wound, enemy_rend, damage):
+# damage : negate damage for dice
+def attack(nb_attack, to_hit, to_wound, rend, enemy_save, damage):
 	attack_hit = 0
-	for attack in range(nb_attack):
-		if random.randint(1,6) > to_hit:
+	for i in range(nb_attack):
+		if roll(6) > to_hit:
 			attack_hit += 1
-	return attack_hit
+	attack_wound = 0
+	for i in range(attack_hit):
+		if roll(6) > to_wound:
+			attack_wound += 1
+	for i in range(attack_wound):
+		if roll(6) - rend > enemy_save:
+			attack_wound -= 1
+	total_damage = 0
+	for i in range(attack_wound):
+		if damage < 0:
+			dgt = roll(-damage)
+		else:
+			dgt = damage
+		total_damage += dgt
+	return total_damage
 
-print(attack(3, 3, 0, 0, 0))
+def roll(dice):
+	return random.randint(1,dice)
+
+def endringmaster_round_var1(enemy_save):
+	aethercannon = attack(1,3,2,2,enemy_save,-3)
+	suit_weapon = attack(6,3,3,1,enemy_save,1)
+	gaze = attack(1,3,2,1,enemy_save,-3)
+	return aethercannon + suit_weapon + gaze
+
+def endringmaster_plot(nb_try):
+	wounds = []
+	for i in range(nb_try):
+		wounds.append(endringmaster_round_var1(6))
+
+	plt.boxplot(wounds)
+	plt.title('Endringmaster')
+	plt.show()
+
+
+endringmaster_plot(100000)
